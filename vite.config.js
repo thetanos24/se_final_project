347853504c8208d99ad39d53600ad91b10114a89
Karen Tano <thetanos24@gmail.com>
2026-04-22 17:32:53 -0700

    updated dist/index.html

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/se_final_project/",
   plugins: [react()],
   server: {
     port: 5173,
